2db1390c5fc756de31af1c920d6e8483c4254f25
Michał Miłek <michal.mileq@gmail.com>
2025-03-31 22:38:17 +0200

    Add goroutine cancellation

---
+++ backend/uptime/run.go
@@ -0,0 +1,23 @@
+package uptime
+
+import (
+	"context"
+	"github.com/m-milek/leszmonitor/logger"
+	"time"
+)
+
+func StartUptimeWorker(ctx context.Context) {
+	logger.Uptime.Info().Msg("Starting uptime worker...")
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Uptime.Info().Msg("Uptime worker shutting down...")
+			return
+		case <-ticker.C:
+			logger.Uptime.Trace().Msg("Uptime worker is running...")
+		}
+	}
+}
