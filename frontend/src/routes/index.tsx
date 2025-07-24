e36dd66d1501bdbc668f38e5146232f09a6515ec
Michał Miłek <michal.mileq@gmail.com>
2025-07-24 20:45:50 +0200

    10: Remove files

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import {createFileRoute, Navigate} from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <Navigate to="/dashboard" replace={true} />
+  )
+}
