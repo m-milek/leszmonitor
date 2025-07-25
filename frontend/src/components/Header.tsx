74978cbeb2cab862034141ff28319e570dee0543
Michał Miłek <michal.mileq@gmail.com>
2025-07-25 02:15:05 +0200

    10: Migrate to tanstack router

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import {Burger} from "@mantine/core";
+
+interface HeaderProps {
+  toggle?: () => void
+  opened?: boolean;
+}
+
+export default function Header({toggle, opened}: Readonly<HeaderProps>) {
+  return (
+    <header>
+      <nav>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm"/>
+        Leszmonitor
+      </nav>
+    </header>
+  )
+}
