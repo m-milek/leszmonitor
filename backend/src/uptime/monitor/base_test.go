22af195d5228ce7d781130d7acd3cb3c49d305a9
Michał Miłek <michal.mileq@gmail.com>
2025-08-16 08:18:04 +0200

    31: Fix tests

--- backend/src/uptime/monitor/base_test.go
+++ backend/src/uptime/monitor/base_test.go
@@ -1,14 +1,16 @@
 package monitors
 
 import (
+	"github.com/m-milek/leszmonitor/util"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func createTestBaseMonitor() BaseMonitor {
+	name := "Test BaseMonitor"
 	return BaseMonitor{
-		Id:          generateId(),
-		Name:        "Test BaseMonitor",
+		Id:          util.IdFromString(name),
+		Name:        name,
 		Description: "Test Description",
 		Interval:    60,
 		Type:        Http,
@@ -67,29 +69,5 @@ func TestBaseMonitorGenerateId(t *testing.T) {
 		originalId := monitor.Id
 		monitor.GenerateId()
 		assert.Equal(t, originalId, monitor.Id, "ID should remain unchanged if already set")
-	})
-}
-
-func TestGenerateMonitorId(t *testing.T) {
-	t.Run("Valid ID Generation", func(t *testing.T) {
-		id := generateId()
-		assert.NotEmpty(t, id, "Generated ID should not be empty")
-	})
-
-	t.Run("ID Uniqueness", func(t *testing.T) {
-		id1 := generateId()
-		id2 := generateId()
-		assert.NotEqual(t, id1, id2, "Generated IDs should be unique")
-	})
-
-	t.Run("Id generated multiple times is different", func(t *testing.T) {
-		ids := make(map[string]bool)
-		for i := 0; i < 100; i++ {
-			id := generateId()
-			assert.NotEmpty(t, id, "Generated ID should not be empty")
-			_, exists := ids[id]
-			assert.False(t, exists, "Generated ID should be unique")
-			ids[id] = true
-		}
 	})
 }
