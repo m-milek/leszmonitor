0555e525491c7e6d4ffe9162524ee34ea952935f
Michał Miłek <michal.mileq@gmail.com>
2025-12-26 22:42:14 +0100

    42: DB Improvements

--- backend/src/db/group_repository.go
+++ backend/src/db/group_repository.go
@@ -3,21 +3,30 @@ package db
 import (
 	"context"
 	"errors"
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
 	"github.com/m-milek/leszmonitor/logging"
 	"github.com/m-milek/leszmonitor/models"
 )
 
 type IGroupRepository interface {
-	CreateMonitorGroup(ctx context.Context, group *models.MonitorGroup) (*models.MonitorGroup, error)
-	GetMonitorGroupByID(ctx context.Context, displayID string) (*models.MonitorGroup, error)
-	GetMonitorGroupsForTeam(ctx context.Context, team *models.Team) ([]models.MonitorGroup, error)
-	UpdateMonitorGroup(ctx context.Context, team *models.Team, oldGroup, newGroup *models.MonitorGroup) (bool, error)
-	DeleteMonitorGroup(ctx context.Context, team *models.Team, groupID string) (bool, error)
+	InsertGroup(ctx context.Context, group *models.MonitorGroup) error
+	GetGroupByDisplayID(ctx context.Context, displayID string) (*models.MonitorGroup, error)
+	GetGroupsByTeamID(ctx context.Context, team *models.Team) ([]models.MonitorGroup, error)
+	UpdateGroup(ctx context.Context, team *models.Team, oldGroup, newGroup *models.MonitorGroup) (bool, error)
+	DeleteGroup(ctx context.Context, team *models.Team, groupID string) (bool, error)
 }
 
 type groupRepository struct {
 	baseRepository
+}
+
+// groupFromCollectableRow maps a pgx.CollectableRow to a models.MonitorGroup struct.
+func groupFromCollectableRow(row pgx.CollectableRow) (models.MonitorGroup, error) {
+	group := models.MonitorGroup{}
+	err := row.Scan(&group.ID, &group.TeamID, &group.DisplayID, &group.Name, &group.Description, &group.CreatedAt, &group.UpdatedAt)
+
+	return group, err
 }
 
 func newGroupRepository(repository baseRepository) IGroupRepository {
@@ -26,42 +35,42 @@ func newGroupRepository(repository baseRepository) IGroupRepository {
 	}
 }
 
-func (r *groupRepository) CreateMonitorGroup(ctx context.Context, group *models.MonitorGroup) (*models.MonitorGroup, error) {
-	return dbWrap(ctx, "CreateMonitorGroup", func() (*models.MonitorGroup, error) {
-		rows := r.pool.QueryRow(ctx,
-			`INSERT INTO groups (display_id, team_id, name, description) VALUES ($1, $2, $3, $4) RETURNING id`,
-			group.DisplayID, group.TeamID, group.Name, group.Description)
-
-		err := rows.Scan(&group.ID)
-
-		return group, err
+func (r *groupRepository) InsertGroup(ctx context.Context, group *models.MonitorGroup) error {
+	_, err := dbWrap(ctx, "InsertGroup", func() (*any, error) {
+		_, err := r.pool.Exec(ctx,
+			`INSERT INTO groups (team_id, display_id, name, description) VALUES ($1, $2, $3, $4)`,
+			group.TeamID, group.DisplayID, group.Name, group.Description)
+		return nil, err
 	})
+	if pgErrIs(err, pgerrcode.UniqueViolation) {
+		return ErrAlreadyExists
+	}
+	return err
 }
 
-func (r *groupRepository) GetMonitorGroupByID(ctx context.Context, displayID string) (*models.MonitorGroup, error) {
-	return dbWrap(ctx, "GetMonitorGroupByID", func() (*models.MonitorGroup, error) {
-		rows := r.pool.QueryRow(ctx,
-			`SELECT id, display_id, team_id, name, description, created_at, updated_at FROM groups WHERE display_id=$1`,
+func (r *groupRepository) GetGroupByDisplayID(ctx context.Context, displayID string) (*models.MonitorGroup, error) {
+	return dbWrap(ctx, "GetGroupByDisplayID", func() (*models.MonitorGroup, error) {
+		row, err := r.pool.Query(ctx,
+			`SELECT id, team_id, display_id, name, description, created_at, updated_at FROM groups WHERE display_id=$1`,
 			displayID)
 
-		group := &models.MonitorGroup{}
-		err := rows.Scan(&group.ID, &group.DisplayID, &group.TeamID, &group.Name, &group.Description, &group.CreatedAt, &group.UpdatedAt)
-
 		if err != nil {
-			if errors.Is(err, pgx.ErrNoRows) {
-				return nil, ErrNotFound
-			}
 			return nil, err
 		}
 
-		return group, err
+		group, err := pgx.CollectExactlyOneRow(row, groupFromCollectableRow)
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+
+		return &group, err
 	})
 }
 
-func (r *groupRepository) GetMonitorGroupsForTeam(ctx context.Context, team *models.Team) ([]models.MonitorGroup, error) {
-	return dbWrap(ctx, "GetMonitorGroupsByTeamID", func() ([]models.MonitorGroup, error) {
+func (r *groupRepository) GetGroupsByTeamID(ctx context.Context, team *models.Team) ([]models.MonitorGroup, error) {
+	return dbWrap(ctx, "GetGroupsByTeamID", func() ([]models.MonitorGroup, error) {
 		rows, err := r.pool.Query(ctx,
-			`SELECT id, display_id, team_id, name, description, created_at, updated_at FROM groups WHERE team_id=$1`,
+			`SELECT id, team_id, display_id, name, description, created_at, updated_at FROM groups WHERE team_id=$1`,
 			team.ID)
 		if err != nil {
 			if errors.Is(err, pgx.ErrNoRows) {
@@ -72,11 +81,7 @@ func (r *groupRepository) GetMonitorGroupsForTeam(ctx context.Context, team *mod
 		}
 
 		var groups []models.MonitorGroup
-		groups, err = pgx.CollectRows(rows, func(row pgx.CollectableRow) (models.MonitorGroup, error) {
-			var group models.MonitorGroup
-			err := row.Scan(&group.ID, &group.DisplayID, &group.TeamID, &group.Name, &group.Description, &group.CreatedAt, &group.UpdatedAt)
-			return group, err
-		})
+		groups, err = pgx.CollectRows(rows, groupFromCollectableRow)
 		if err != nil {
 			return nil, err
 		}
@@ -84,8 +89,8 @@ func (r *groupRepository) GetMonitorGroupsForTeam(ctx context.Context, team *mod
 	})
 }
 
-func (r *groupRepository) UpdateMonitorGroup(ctx context.Context, team *models.Team, oldGroup, newGroup *models.MonitorGroup) (bool, error) {
-	return dbWrap(ctx, "UpdateMonitorGroup", func() (bool, error) {
+func (r *groupRepository) UpdateGroup(ctx context.Context, team *models.Team, oldGroup, newGroup *models.MonitorGroup) (bool, error) {
+	return dbWrap(ctx, "UpdateGroup", func() (bool, error) {
 		result, err := r.pool.Exec(ctx,
 			`UPDATE groups SET display_id=$1, name=$2, description=$3 WHERE id=$4 AND team_id=$5 RETURNING *`,
 			newGroup.DisplayID, newGroup.Name, newGroup.Description, oldGroup.ID, team.ID)
@@ -101,8 +106,8 @@ func (r *groupRepository) UpdateMonitorGroup(ctx context.Context, team *models.T
 	})
 }
 
-func (r *groupRepository) DeleteMonitorGroup(ctx context.Context, team *models.Team, groupID string) (bool, error) {
-	return dbWrap(ctx, "DeleteMonitorGroup", func() (bool, error) {
+func (r *groupRepository) DeleteGroup(ctx context.Context, team *models.Team, groupID string) (bool, error) {
+	return dbWrap(ctx, "DeleteGroup", func() (bool, error) {
 		result, err := r.pool.Exec(ctx,
 			`DELETE FROM groups WHERE display_id=$1 AND team_id=$2`,
 			groupID, team.ID)
