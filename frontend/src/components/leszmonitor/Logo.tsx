c78cbe8180180d6562fe3908d4279a42df015dc9
Michał Miłek <michal.mileq@gmail.com>
2026-02-08 20:19:22 +0100

    Login page

---
+++ frontend/src/components/leszmonitor/Logo.tsx
@@ -0,0 +1,6 @@
+export const Logo = () => (
+  <div className="flex items-center">
+    <img src="/logo.svg" alt="Leszmonitor Logo" className="w-8 h-8 mr-2" />
+    <h1 className="text-lg font-bold">Leszmonitor</h1>
+  </div>
+);
