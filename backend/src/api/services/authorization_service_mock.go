81b65971d69b5d1f5562d52271621200e423f247
Michał Miłek <michal.mileq@gmail.com>
2026-03-23 20:31:34 +0100

    Fix sonar issue

--- backend/src/api/services/authorization_service_mock.go
+++ backend/src/api/services/authorization_service_mock.go
@@ -2,18 +2,19 @@ package services
 
 import (
 	"context"
+
 	"github.com/m-milek/leszmonitor/api/middleware"
 	"github.com/m-milek/leszmonitor/models"
 	"github.com/stretchr/testify/mock"
 )
 
-// IAuthorizationService defines the interface for authorization operations.
+// ITeamActionAuthorizer defines the interface for authorization operations.
 // This interface allows for easy mocking in tests.
-type IAuthorizationService interface {
+type ITeamActionAuthorizer interface {
 	authorizeTeamAction(ctx context.Context, teamAuth *middleware.TeamAuth, permissions ...models.Permission) (*models.Team, *ServiceError)
 }
 
-// MockAuthorizationService is a mock implementation of IAuthorizationService for testing.
+// MockAuthorizationService is a mock implementation of ITeamActionAuthorizer for testing.
 type MockAuthorizationService struct {
 	mock.Mock
 }
