237621c1101969bb1d8d82ed4d07206c54baba09
Michał Miłek <michal.mileq@gmail.com>
2026-04-25 23:08:46 +0200

    WebSocket connection stable

--- backend/src/workers/websocket.go
+++ backend/src/workers/websocket.go
@@ -4,52 +4,34 @@ import (
 	"context"
 
 	"github.com/gorilla/websocket"
-	"github.com/m-milek/leszmonitor/api/middleware"
-	"github.com/m-milek/leszmonitor/auth"
 	"github.com/m-milek/leszmonitor/log"
 )
 
 func RunWebSocketWorker(ctx context.Context, conn *websocket.Conn) {
-	defer conn.Close()
+	defer func() {
+		_ = conn.Close()
+	}()
 
 	log.Api.Info().Any("remoteAddr", conn.RemoteAddr()).Msg("WebSocket connection established")
-
-	// First message auth
-	_, message, err := conn.ReadMessage()
-	if err != nil {
-		log.Api.Error().Err(err).Msg("Failed to read authentication message from WebSocket connection")
-		return
-	}
-
-	authToken := string(message)
-	userClaims, err := auth.ValidateJwt(authToken)
-	if err != nil {
-		log.Api.Error().Err(err).Msg("WebSocket client authentication failed")
-		return
-	}
-
-	ctx = middleware.SetUserContext(ctx, userClaims)
-
-	log.Api.Info().Any("remoteAddr", conn.RemoteAddr()).Msg("WebSocket client authenticated successfully")
-
-	err = conn.WriteMessage(websocket.TextMessage, []byte("OK"))
-	if err != nil {
-		log.Api.Error().Err(err).Msg("Failed to send authentication success message to WebSocket client")
-		return
-	}
 
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			_, message, err := conn.ReadMessage()
+			messageType, message, err := conn.ReadMessage()
 			if err != nil {
 				return
 			}
 
-			err = conn.WriteMessage(websocket.TextMessage, message)
-			if err != nil {
+			if messageType == websocket.TextMessage && string(message) == "ping" {
+				if err := conn.WriteMessage(websocket.TextMessage, []byte("pong")); err != nil {
+					return
+				}
+				continue
+			}
+
+			if err := conn.WriteMessage(messageType, message); err != nil {
 				return
 			}
 		}
