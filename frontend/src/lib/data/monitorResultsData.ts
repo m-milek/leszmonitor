f9cc96db6739ba2601869609c7bd6f7f6be673e2
Michał Miłek <michal.mileq@gmail.com>
2026-05-05 00:29:28 +0200

    77: Monitor result list in monitor view

--- frontend/src/lib/data/monitorResultsData.ts
+++ frontend/src/lib/data/monitorResultsData.ts
@@ -1,8 +1,8 @@
 import { authFetch } from "@/lib/data/utils.ts";
 import { BACKEND_API_URL } from "@/lib/consts.ts";
-import type { MonitorResult } from "@/lib/types.ts";
+import type { MonitorResult, Pagination } from "@/lib/types.ts";
 
-export const getLatestMonitorResultById = async (
+export const getLatestMonitorResultByMonitorId = async (
   monitorId: string,
 ): Promise<MonitorResult | null> => {
   const res = await authFetch(
@@ -10,7 +10,26 @@ export const getLatestMonitorResultById = async (
   );
 
   if (res.status === 404) return null;
-  if (!res.ok) throw new Error(`Failed to fetch result for ${monitorId}`);
+  if (!res.ok)
+    throw new Error(`Failed to fetch latest result for ${monitorId}`);
+
+  return res.json();
+};
+
+export const getMonitorResultsByMonitorId = async (
+  monitorId: string,
+  pagination: Pagination,
+): Promise<MonitorResult[] | null> => {
+  const queryParams = new URLSearchParams({
+    page: pagination.page.toString(),
+    per_page: pagination.perPage.toString(),
+  });
+  const res = await authFetch(
+    `${BACKEND_API_URL}/monitors/${monitorId}/results?${queryParams.toString()}`,
+  );
+
+  if (res.status === 404) return null;
+  if (!res.ok) throw new Error(`Failed to fetch results for ${monitorId}`);
 
   return res.json();
 };
